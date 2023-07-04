ec8684c95868500a06c9a73074c6babed5fec7a9
Aman Kumar <rathoraman1203@gmail.com>
2023-07-04 21:00:10 +0530

    update js and css url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
     <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
     <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="../assets/css/demo.css" />
 
@@ -1167,22 +1167,22 @@
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    <script src="{{url('/assets/vendor/libs/jquery/jquery.js')}}"></script>
-    <script src="{{url('/assets/vendor/libs/popper/popper.js')}}"></script>
-    <script src="{{url('/assets/vendor/js/bootstrap.js')}}"></script>
-    <script src="{{url('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{ url('/assets/vendor/libs/jquery/jquery.js')}}"></script>
+    <script src="{{ url('/assets/vendor/libs/popper/popper.js')}}"></script>
+    <script src="{{ url('/assets/vendor/js/bootstrap.js')}}"></script>
+    <script src="{{ url('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
 
-    <script src="{{url('/assets/vendor/js/menu.js')}}"></script>
+    <script src="{{ url('/assets/vendor/js/menu.js')}}"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{url('/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+    <script src="{{ url('/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
 
     <!-- Main JS -->
-    <script src="{{url('/assets/js/main.js')}}"></script>
+    <script src="{{ url('/assets/js/main.js')}}"></script>
 
     <!-- Page JS -->
-    <script src="{{url('/assets/js/dashboards-analytics.js')}}"></script>
+    <script src="{{ url('/assets/js/dashboards-analytics.js')}}"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
